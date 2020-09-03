ee86e02e84526c9380870ce54393ae7bd3f7dc8d
Naofumi Okamoto <nokamoto.engr@gmail.com>
2020-09-03 17:44:30 +0900

    Add api.json (#12)

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,5 +6,9 @@ DIRECTORIES=$(find . -name *.proto | xargs -I {} dirname {} | sort | uniq)
 
 for DIRECTORY in ${DIRECTORIES};
 do
-    protoc --go_out=plugins=grpc,paths=source_relative:. -I . ${DIRECTORY}/*.proto
+    protoc \
+        --go_out=plugins=grpc,paths=source_relative:. \
+        --authz_out=paths=source_relative:. \
+        -I . \
+        ${DIRECTORY}/*.proto
 done
